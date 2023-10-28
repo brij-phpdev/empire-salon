8198eae6ed2e42cc9493779b8c4c9cdce0cbc739
shriradharani <brij.phpdev@gmail.com>
2023-10-28 20:01:25 +0530

    fix about us team

--- about-us.php
+++ about-us.php
@@ -40,83 +40,113 @@
                    <h2>Our Services</h2>
                    <div class="heading-line"></div>
                 </div>
-                <div class="row">
-                    <div class="col-lg-3 col-md-6 sm-padding wow fadeInUp" data-wow-delay="200ms">
-                        <div class="service_box">
-                            <i class="bs bs-scissors-1"></i>
-                            <h3>Haircut Styles</h3>
-                            <p>Barber is a person whose occupation is mainly to cut dress style.</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 sm-padding wow fadeInUp" data-wow-delay="300ms">
-                        <div class="service_box">
-                            <i class="bs bs-razor-2"></i>
-                            <h3>Beard Triming</h3>
-                            <p>Barber is a person whose occupation is mainly to cut dress style.</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 sm-padding wow fadeInUp" data-wow-delay="400ms">
-                        <div class="service_box">
-                            <i class="bs bs-brush"></i>
-                            <h3>Smooth Shave</h3>
-                            <p>Barber is a person whose occupation is mainly to cut dress style.</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 sm-padding wow fadeInUp" data-wow-delay="500ms">
-                        <div class="service_box">
-                            <i class="bs bs-hairbrush-1"></i>
-                            <h3>Face Masking</h3>
-                            <p>Barber is a person whose occupation is mainly to cut dress style.</p>
-                        </div>
-                    </div>
+                <div id="trending_cuts_carousel" class="row ">
+            <div class="col-lg-12 col-md-8 sm-padding">
+                <div class="service_content align-center wow fadeInUp" data-wow-delay="200ms">
+                    <img src="img/hairstyles/1.jpg" alt="Services">
+                    <h3>Cut & Style</h3>
+                    <P class="text-justify">Experience the uniqueness of hair cutting & styling in Agra. Our expert team uses signature techniques to craft a personalized style that perfectly matches your personality, lifestyle, and hair type.</P>
                 </div>
             </div>
+            <div class="col-lg-12 col-md-8 sm-padding wow fadeInUp" data-wow-delay="300ms">
+                <div class="service_content align-center">
+                    <img src="img/hairstyles/2.jpg" alt="Services">
+                    <h3>Signature Cut</h3>
+                    <P class="text-justify">Elevate your style with our skilled hair artisans, crafting the flawless EMPIRE signature cut, tailored just for you. Indulge in a stunning blow-dry, enriched by our premium label. haircare range.</P>
+                </div>
+            </div>
+            <div class="col-lg-12 col-md-8 sm-padding wow fadeInUp" data-wow-delay="400ms">
+                <div class="service_content align-center">
+                    <img src="img/hairstyles/3.jpg" alt="Services">
+                    <h3>Barbering</h3>
+                    <P class="text-justify">For a swift, customized EMPIRE signature barbered cut, trust our experts. A quick and simple finish, perfectly suited to you, enriched with the finesse of our professional label. haircare products.</P>
+                </div>
+            </div>
+            <div class="col-lg-12 col-md-8 sm-padding wow fadeInUp" data-wow-delay="500ms">
+                <div class="service_content align-center">
+                    <img src="img/hairstyles/4.jpg" alt="Services">
+                    <h3>Afro Textured Hair</h3>
+                    <P class="text-justify">Celebrate your unique natural textures or opt for a relaxing transformation. Our artistry creates exquisite, edgy yet practical shapes, enriched with colors that nourish, fortify, and enhance those beautiful curls.</P>
+                </div>
+            </div>
+            <div class="col-lg-12 col-md-8 sm-padding wow fadeInUp" data-wow-delay="500ms">
+                <div class="service_content align-center">
+                    <img src="img/hairstyles/5.jpg" alt="Services">
+                    <h3>COLORING</h3>
+                    <P class="text-justify">Our color specialist will tune in to your color aspirations and requirements, all while evaluating your skin tone, eye shade, and hair texture. This personalized approach leads to the ideal EMPIRE signature hue for you in Agra.</P>
+                </div>
+            </div>
+            <div class="col-lg-12 col-md-8 sm-padding wow fadeInUp" data-wow-delay="500ms">
+                <div class="service_content align-center">
+                    <img src="img/hairstyles/6.jpg" alt="Services">
+                    <h3>Signature Colour</h3>
+                    <P class="text-justify">Experience an EMPIRE full head color transformation, spanning from roots to ends. Whether you desire enhanced shine, lightening, darkening, tone alteration, or white hair coverage, our color experts have the perfect solution for you.</P>
+                </div>
+            </div>
+            <div class="col-lg-12 col-md-8 sm-padding wow fadeInUp" data-wow-delay="500ms">
+                <div class="service_content align-center">
+                    <img src="img/hairstyles/7.jpg" alt="Services">
+                    <h3>Signature Balayage</h3>
+                    <P class="text-justify">Indulge in an EMPIRE custom, full head application of freehand balayage. Achieve the natural, sun-kissed look or opt for striking, face-framing highlights to suit your style.</P>
+                </div>
+            </div>
+            <div class="col-lg-12 col-md-8 sm-padding wow fadeInUp" data-wow-delay="500ms">
+                <div class="service_content align-center">
+                    <img src="img/hairstyles/8.jpeg" alt="Services">
+                    <h3>Signature Highlights</h3>
+                    <P class="text-justify">EMPIRE's timeless highlights cascade through your hair, illuminating it with a multi-tonal effect. They can be natural, textured, or boldly vibrant, making them an excellent choice for blending white hair.</P>
+                </div>
+            </div>
+        </div>
+            </div>
         </section><!--/. service_section -->
+
+<?php
+$agent_array = array();
+$agenttable_sql = "SELECT * FROM `agents`";
+if ($agenttable_res = mysqli_query($link, $agenttable_sql)) {
+    if (mysqli_num_rows($agenttable_res) > 0) {
+        while ($agenttable_row = mysqli_fetch_assoc($agenttable_res)) {
+            $agent_array[] = $agenttable_row;
+        }
+    }
+}
+?>
         
         <section id="team" class="team_section bd-bottom padding">
             <div class="container">
                 <div class="section_heading text-center mb-40 wow fadeInUp" data-wow-delay="300ms">
                    <h3>Trendy Salon &amp; Spa</h3>
-                   <h2>Our Barbers</h2>
+                   <h2>Our Stylists</h2>
                    <div class="heading-line"></div>
                 </div>
                 <ul class="team_members row">
-                    <li class="col-lg-3 col-md-6 sm-padding wow fadeInUp" data-wow-delay="200ms">
+                    <?php
+            if ($agent_array):
+                $data_wow_delay = 2;
+                ?>
+                <?php
+                foreach ($agent_array as $agent):
+
+                    $path = SITE_BOOK_URL . '/application/uploads/img/agents/' . $agent['agentImage'];
+                    $type = pathinfo($path, PATHINFO_EXTENSION);
+                    $data = file_get_contents($path);
+                    $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+                    ?>
+                    <li class="col-lg-3 col-md-6 sm-padding wow fadeInUp" data-wow-delay="<?php echo $data_wow_delay ?>00ms">
                         <div class="team_member">
-                            <img src="img/team-1.jpg" alt="Team Member">
+                            <img src="<?php echo $base64 ?>" alt="Stylist <?php echo $agent['agentName'] ?>">
                             <div class="overlay">
-                                <h3>Kyle Frederick</h3>
-                                <p>WEB DESIGNER</p>
+                                <h3><?php echo $agent['agentName'] ?></h3>
+                                <p><?php echo substr(html_entity_decode($agent['agentDetail']), 0, 30) ?>...</p>
                             </div>
                         </div>
                     </li>
-                    <li class="col-lg-3 col-md-6 sm-padding wow fadeInUp" data-wow-delay="300ms">
-                        <div class="team_member">
-                            <img src="img/team-2.jpg" alt="Team Member">
-                            <div class="overlay">
-                                <h3>José Carpio</h3>
-                                <p>WORDPRESS DEVELOPER</p>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="col-lg-3 col-md-6 sm-padding wow fadeInUp" data-wow-delay="400ms">
-                        <div class="team_member">
-                            <img src="img/team-3.jpg" alt="Team Member">
-                            <div class="overlay">
-                                <h3>Michel Ibáñez</h3>
-                                <p>ONLINE MARKETER</p>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="col-lg-3 col-md-6 sm-padding wow fadeInUp" data-wow-delay="500ms">
-                        <div class="team_member">
-                            <img src="img/team-4.jpg" alt="Team Member">
-                            <div class="overlay">
-                                <h3>Adam Castellon</h3>
-                                <p>JAVA SPECIALIST</p>
-                            </div>
-                        </div>
-                    </li>
+                    <?php
+                    $data_wow_delay++;
+                endforeach;
+            endif;
+            ?>
                 </ul><!-- /.team_members -->
             </div>
         </section><!-- /.team_section -->
