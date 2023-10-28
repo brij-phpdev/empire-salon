bb4f89e030c30601b21a8f9f9a1cdb593f2a05b2
shriradharani <brij.phpdev@gmail.com>
2023-10-28 19:41:27 +0530

    remove our stylist

--- index.php
+++ index.php
@@ -209,7 +209,7 @@ if ($agenttable_res = mysqli_query($link, $agenttable_sql)) {
 }
 ?>
 
-<section id="team" class="team_section bd-bottom padding">
+<section id="team" class="team_section bd-bottom padding d-none">
     <div class="container">
         <div class="section_heading text-center mb-40 wow fadeInUp" data-wow-delay="300ms">
             <h3>Trendy Salon &amp; Spa</h3>
