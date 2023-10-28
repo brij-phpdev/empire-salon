b910454e4ac45cc201ad89eeb9f13ce465496579
shriradharani <brij.phpdev@gmail.com>
2023-10-28 20:19:55 +0530

    remove loader

--- includes/header.php
+++ includes/header.php
@@ -53,11 +53,11 @@ require_once( './couch-admin/cms.php' );
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-        <div id='preloader' >
+<!--        <div id='preloader' >
             <div class='loader' >
                 <img src="img/loading.gif" width="80" alt="">
             </div>
-        </div><!-- Preloader -->
+        </div> Preloader -->
 
         <header id="header" class="header-section">
             <div class="container">
