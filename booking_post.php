32fea9f887f46eb047d2fc842555370cc83c7f3d
shriradharani <brij.phpdev@gmail.com>
2023-10-29 12:30:24 +0530

    fix booking

--- booking_post.php
+++ booking_post.php
@@ -1,11 +1,11 @@
 <?php
+
 //error_reporting(E_ALL);
 include_once './includes/config.php';
 include_once './includes/database.php';
 
 //print_r($_POST);
 //die;
-
 // lets check if user exists if not then create user first... to pick the id
 
 $name = trim($_POST['name']);
@@ -16,7 +16,7 @@ $other_services = serialize($_POST['other_services']);
 $agentId = trim($_POST['agentId']);
 $adults = trim($_POST['serviceAdult']);
 $childrens = trim($_POST['serviceChildren']);
-$date = date('m-d-Y',strtotime(trim($_POST['date'])));
+$date = date('m-d-Y', strtotime(trim($_POST['date'])));
 $timing = trim($_POST['select_time']);
 $message = trim($_POST['message']);
 $serviceBill = '0';
@@ -35,65 +35,63 @@ $check_user_sql = "SELECT * FROM `logintbl` WHERE `email`='$email' AND `phone`='
 //$res = mysqli_query($link, $check_user_sql);
 //var_dump($res);die;
 if ($res = mysqli_query($link, $check_user_sql)) {
-        if (mysqli_num_rows($res) > 0) {
-            while ($row = mysqli_fetch_assoc($res)) {
-                $userId = $row['id'];
-            }
-        }else{
-    // insert 
-
-    $userPwd = generatePassword();
-    $userLoginPwd = md5($userPwd);
-    $insert_user_sql = "INSERT INTO `logintbl` (`id`,`fullName`,`email`,`password`,`verifiedEmail`,`image`,`photoURL`,"
-            . "`role`,`phone`,`gender`,`bookingId`,`activated`,`google`,`facebook`,`privacy`,`register_date`)  "
-            . "VALUES (NULL,'$name','$email','$userLoginPwd',0,'','',"
-            . "'0','$phone','','0','0','0','0','0','$upload_date') ";  
-         
-    $exe = mysqli_query($link,$insert_user_sql);
-    if($exe){
-    
-        $userId = mysqli_insert_id($link);
-    }else{
-        echo "ERROR: Some error occured while registering user. "
-                                .mysqli_error($link); 
-    }
-    
+    if (mysqli_num_rows($res) > 0) {
+        while ($row = mysqli_fetch_assoc($res)) {
+            $userId = $row['id'];
         }
-}
-mysqli_free_result($res); 
+    } else {
+        // insert 
 
+        $userPwd = generatePassword();
+        $userLoginPwd = md5($userPwd);
+        $insert_user_sql = "INSERT INTO `logintbl` (`id`,`fullName`,`email`,`password`,`verifiedEmail`,`image`,`photoURL`,"
+                . "`role`,`phone`,`gender`,`bookingId`,`activated`,`google`,`facebook`,`privacy`,`register_date`)  "
+                . "VALUES (NULL,'$name','$email','$userLoginPwd',0,'','',"
+                . "'0','$phone','','0','0','0','0','0','$upload_date') ";
+
+        $exe = mysqli_query($link, $insert_user_sql);
+        if ($exe) {
+
+            $userId = mysqli_insert_id($link);
+        } else {
+            echo "ERROR: Some error occured while registering user. "
+            . mysqli_error($link);
+        }
+    }
+}
+mysqli_free_result($res);
 
 // now insert into booking table..
-     $insert_booking_sql = "INSERT INTO `bookingtbl` "
-             . "(`id`,`serviceId`,`agentId`,`adults`,`childrens`,`date`,`timing`,`message`,`serviceBill`,`paymentStatus`,"
-             . "`orderId`,`serviceStatus`,`userId`,`upload_date`) "
-             . " VALUES (NULL,'$serviceId','$agentId','$adults','$childrens','$date','$timing','$message','$serviceBill','$paymentStatus',"
-             . "'$orderId','$serviceStatus','$userId','$upload_date') "
-             . " ";       
-        
-    $exe = mysqli_query($link,$insert_booking_sql);
-    if($exe){
-    
-        $bookingId = mysqli_insert_id($link);
-    }else{
-        echo "ERROR: Some error occured while booking. "
-                                .mysqli_error($link); 
-    }
-    if($bookingId){
-        echo "An appointment is fixed";
+$insert_booking_sql = "INSERT INTO `bookingtbl` "
+        . "(`id`,`serviceId`,`agentId`,`adults`,`childrens`,`date`,`timing`,`message`,`serviceBill`,`paymentStatus`,"
+        . "`orderId`,`serviceStatus`,`userId`,`upload_date`) "
+        . " VALUES (NULL,'$serviceId','$agentId','$adults','$childrens','$date','$timing','$message','$serviceBill','$paymentStatus',"
+        . "'$orderId','$serviceStatus','$userId','$upload_date') "
+        . " ";
+
+$exe = mysqli_query($link, $insert_booking_sql);
+if ($exe) {
+
+    $bookingId = mysqli_insert_id($link);
+} else {
+    echo "ERROR: Some error occured while booking. "
+    . mysqli_error($link);
+}
+if ($bookingId) {
+    echo "An appointment is fixed";
 //        echo json_encode(['result'=>'success']);
-        die;
-    }
+    die;
+}
 
-     
-    function generatePassword($length = 8) {
-        $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-        $count = mb_strlen($chars);
+function generatePassword($length = 8) {
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    $count = mb_strlen($chars);
 
-        for ($i = 0, $result = ''; $i < $length; $i++) {
-            $index = rand(0, $count - 1);
-            $result .= mb_substr($chars, $index, 1);
-        }
-        return $result;
+    for ($i = 0, $result = ''; $i < $length; $i++) {
+        $index = rand(0, $count - 1);
+        $result .= mb_substr($chars, $index, 1);
     }
+    return $result;
+}
+
 ?>
