bf3e49f0a1ed2266663a2b5919d07128dabc0dd0
shriradharani <brij.phpdev@gmail.com>
2023-10-30 23:49:09 +0530

    update birdal banner caption

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ include_once './includes/database.php';
             <ul id="main-slider" class="owl-carousel main_slider">
                 <li class="main_slide d-flex align-items-center">
                     <video id="background-video" muted loop="loop" autoplay="autoplay" poster="img/headers/the-makeup-artist-works-in-her-salon-applies-prof.jpg">
-                        <source src="videos/close-up-of-female-hairdresser-makes-hairstyle.mp4" type="video/mp4">
-                        <source src="videos/close-up-of-female-hairdresser-makes-hairstyle.webm" type="video/webm">
+                        <source src="videos/Bride-Getting-Ready.mp4" type="video/mp4">
+                        <!--<source src="videos/close-up-of-female-hairdresser-makes-hairstyle.webm" type="video/webm">-->
 <!--                        <source src="movie.ogg" type="video/ogg">-->
                         Your browser does not support the video tag.
                     </video>
