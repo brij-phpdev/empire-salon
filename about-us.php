e9fc157bec17b4a6feecc8de9d8a55c4f8bf3623
shriradharani <brij.phpdev@gmail.com>
2023-10-31 23:59:38 +0530

    about us

--- about-us.php
+++ about-us.php
@@ -1,80 +1,90 @@
-<?php 
+<?php
 include_once( './includes/header.php' );
 include_once( './includes/database.php' );
 ?>
 
-		<section class="page_header d-flex align-items-center">
-		    <div class="container">
-		        <div class="section_heading text-center mb-40 wow fadeInUp" data-wow-delay="300ms">
-                   <h3>Glamour Awaits You Here!</h3>
-                   <h2>The Empire Salon</h2>
-                   
-                </div>
-		    </div>
-		</section><!--/. page_header -->
-        
-        <section class="about_section bd-bottom padding">
-            <div class="container">
-                <div class="row d-flex align-items-center">
-                    <div class="col-md-6 xs-padding wow fadeInLeft" data-wow-delay="300ms">
-                        <div class="section_heading">
-                            <h3>Introducing The Empire Salon</h3>
-                            <h2 style="font-size: 26px;">Pamper yourself with our diverse selection<br> of hair, beauty, and grooming treatments</h2>
-                            <div class="text-justify" style="width: 100%;text-wrap: balance;">At Empire Salon, we pride ourselves on embracing the latest trends in the industry. We're not just a salon; we're a trendsetter. Step into our eco-conscious world where sustainability meets style. Experience the magic of hybrid hair coloring that's tailor-made to accentuate your individuality. Allow us to transport you to the past with vintage-inspired styles that have found their way back into the limelight. Or let your imagination run wild with fantasy colors that paint your hair like a canvas.</div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 xs-padding wow fadeInRight" data-wow-delay="300ms">
-                        <div class="about_video">
-                            <img class="wow fadeInUp img-fluid " data-wow-delay="500ms" src="img/about/our_experts.jpeg" alt="about">
-                        </div>
-                    </div>
+<section class="page_header d-flex align-items-center">
+    <div class="container">
+        <div class="section_heading text-center mb-40 wow fadeInUp" data-wow-delay="300ms">
+            <h3>Glamour Awaits You Here!</h3>
+            <h2>The Empire Salon</h2>
+
+        </div>
+    </div>
+</section><!--/. page_header -->
+
+<section class="about_section bd-bottom padding">
+    <div class="container">
+        <div class="row d-flex align-items-center">
+            <div class="col-md-6 xs-padding wow fadeInLeft" data-wow-delay="300ms">
+                <div class="section_heading">
+                    <h3>Introducing The Empire Salon</h3>
+                    <h2 style="font-size: 26px;">Pamper yourself with our diverse selection<br> of hair, beauty, and grooming treatments</h2>
+                    <div class="text-justify" style="width: 100%;text-wrap: balance;">At Empire Salon, we pride ourselves on embracing the latest trends in the industry. We're not just a salon; we're a trendsetter. Step into our eco-conscious world where sustainability meets style. Experience the magic of hybrid hair coloring that's tailor-made to accentuate your individuality. Allow us to transport you to the past with vintage-inspired styles that have found their way back into the limelight. Or let your imagination run wild with fantasy colors that paint your hair like a canvas.</div>
                 </div>
             </div>
-        </section><!--/. about_section -->
-        <section class="about_section bd-bottom padding">
-            <div class="container">
-                <div class="row d-flex align-items-center">
-                    <div class="col-md-12 xs-padding wow fadeInLeft" data-wow-delay="300ms">
-                        <div class="text-justify">
-                            <p>Empire Salon & Spa is one of Agra's premier and go-to destination for contemporary cutting, coloring, beauty, body and impeccable customer service. Started in year 1928 by the Ahmad family, it is perhaps one of the oldest salons in Agra and existed longer than most of us. Today, this 'empire' of beauty services is one of the grandest, most prestigious salons and spa chain in Agra. At Empire, each client is recognized differently for their individual needs. Each client is delivered with service which is totally tailored to their personal requirements. The handpicked team of talented professional hairdresser, beautician and therapists offers and extensive and exceptional choice of services devoted to the guest satisfaction at the affordable pricing.</p>
-
-                            <p>Over the years, Empire salon has been a pioneer in providing customers with luxurious experience for Hair, Skin, Hands & Feet, Barber Shop and Makeup with best-in-class products, finest amenities and professionally trained staff. With sophisticated establishments at all prime locations, tailored to meet diversified customers.</p>
-
-                            <p>He is an Artist looked-up-to by Hair Stylists across the world for his knowledge of hair, creative cuts, usage of tools and color techniques. Nonetheless, he is a businessman looked-up-to for his vehemence for Empire Salon, making it the most stable and fastest growing high street salon chain in India with world-class amenities.</p>
-                            
-                    </div>
-                    </div>
+            <div class="col-md-6 xs-padding wow fadeInRight" data-wow-delay="300ms">
+                <div class="about_video">
+                    <img class="wow fadeInUp img-fluid " data-wow-delay="500ms" src="img/about/our_experts.jpeg" alt="about">
                 </div>
-                      <div class="row d-flex align-items-center">
-                    <div class="col-md-6 xs-padding wow fadeInLeft" data-wow-delay="300ms">
-                        
-                           
-                            <p>Our team of highly trained professionals are experts in their respective fields, and they are passionate about helping our clients achieve their desired look and feel. We use only the highest quality products and the latest techniques to ensure that our clients receive the best possible service.</p>
+            </div>
+        </div>
+    </div>
+</section><!--/. about_section -->
+<section class="about_section bd-bottom padding">
+    <div class="container">
+        <div class="row d-flex align-items-center">
+            <div class="col-md-12 xs-padding wow fadeInLeft" data-wow-delay="300ms">
+                <div class="text-justify">
+                    <p>Empire Salon & Spa is one of Agra's premier and go-to destination for contemporary cutting, coloring, beauty, body and impeccable customer service. Started in year 1928 by the Ahmad family, it is perhaps one of the oldest salons in Agra and existed longer than most of us. Today, this 'empire' of beauty services is one of the grandest, most prestigious salons and spa chain in Agra. At Empire, each client is recognized differently for their individual needs. Each client is delivered with service which is totally tailored to their personal requirements. The handpicked team of talented professional hairdresser, beautician and therapists offers and extensive and exceptional choice of services devoted to the guest satisfaction at the affordable pricing.</p>
 
-                            <p>At Empire Salon, we understand that every client is unique, and we strive to provide a personalized experience that meets their individual needs. Whether you are looking for a quick touch-up or a full day of pampering, our team will work with you to create a customized plan that addresses your specific needs and preferences.</p>
-
-                            <p>We are committed to providing a safe and comfortable environment for our clients, and we follow strict protocols to ensure that our salon is clean and hygienic at all times. Our goal is to provide a relaxing and rejuvenating experience that leaves you feeling refreshed and renewed. Thank you for considering Empire Salon for your beauty and wellness needs. We look forward to welcoming you to our salon and helping you look and feel your best.</p>
-                        </div>
-                        <div class="col-md-6 d-none d-md-block">
-                        <div class="about_img">
-                            <img src="img/about/1.JPG" alt="idea-images" class="about_img_1 wow fadeInLeft" data-wow-delay="200ms">
-                            <img src="img/about/IMG_5373.JPG" alt="idea-images" class="about_img_2 wow fadeInRight" data-wow-delay="400ms">
-                            <img src="img/about/reception-with-staff.JPG" alt="idea-images" class="about_img_3 wow fadeInLeft" data-wow-delay="600ms">
-                        </div>
-                    </div>
-                    </div>
-                        
-                    </div>
                 </div>
+            </div>
+        </div>
+        <div class="row d-flex align-items-center">
+            <div class="col-md-6 xs-padding wow fadeInLeft" data-wow-delay="300ms">
 
-        </section><!--/. about_section -->
+                <p>Over the years, Empire salon has been a pioneer in providing customers with luxurious experience for Hair, Skin, Hands & Feet, Barber Shop and Makeup with best-in-class products, finest amenities and professionally trained staff. With sophisticated establishments at all prime locations, tailored to meet diversified customers.</p>
 
-        <section class="service_section bg-grey bd-bottom padding">
+                <p>He is an Artist looked-up-to by Hair Stylists across the world for his knowledge of hair, creative cuts, usage of tools and color techniques. Nonetheless, he is a businessman looked-up-to for his vehemence for Empire Salon, making it the most stable and fastest growing high street salon chain in India with world-class amenities.</p>
+
+                <p>Our team of highly trained professionals are experts in their respective fields, and they are passionate about helping our clients achieve their desired look and feel. We use only the highest quality products and the latest techniques to ensure that our clients receive the best possible service.</p>
+
+                <p>At Empire Salon, we understand that every client is unique, and we strive to provide a personalized experience that meets their individual needs. Whether you are looking for a quick touch-up or a full day of pampering, our team will work with you to create a customized plan that addresses your specific needs and preferences.</p>
+
+            </div>
+            <div class="col-md-6 d-none d-md-block">
+                <div class="about_img">
+                    <img src="img/about/1.JPG" alt="idea-images" class="about_img_1 wow fadeInLeft" data-wow-delay="200ms">
+                    <img src="img/about/IMG_5373.JPG" alt="idea-images" class="about_img_2 wow fadeInRight" data-wow-delay="400ms">
+                    <img src="img/about/reception-with-staff.JPG" alt="idea-images" class="about_img_3 wow fadeInLeft" data-wow-delay="600ms">
+                </div>
+            </div>
+        </div>
+        <div class="row d-flex align-items-center">
+            <div class="col-9 xs-padding wow fadeInLeft" data-wow-delay="300ms">
+
+
+                <p>We are committed to providing a safe and comfortable environment for our clients, and we follow strict protocols to ensure that our salon is clean and hygienic at all times. Our goal is to provide a relaxing and rejuvenating experience that leaves you feeling refreshed and renewed. Thank you for considering Empire Salon for your beauty and wellness needs. We look forward to welcoming you to our salon and helping you look and feel your best.</p>
+            </div>
+            <div class="col-3 d-none d-md-block">
+                <div class="about_img">
+                    <img src="img/about/sign.png" alt="idea-images" class="img-fluid wow fadeInLeft" data-wow-delay="200ms">
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+</section><!--/. about_section -->
+
+<section class="service_section bg-grey bd-bottom padding">
     <div class="container">
         <div class="section_heading text-center mb-40 wow fadeInUp" data-wow-delay="300ms">
             <h3>Save 10% On your Next</h3>
             <h2>Our Trending Services</h2>
-            
+
         </div>
         <div id="trending_cuts_carousel_od" class="row ">
             <div class="col-lg-3 col-md-4 sm-padding">
@@ -151,24 +161,24 @@ if ($agenttable_res = @mysqli_query($link, $agenttable_sql)) {
 }
 ?>
 
-        <section id="team" class="team_section bd-bottom padding">
-            <div class="container">
-                <div class="section_heading text-center mb-40 wow fadeInUp" data-wow-delay="300ms">
-                   <h3>Glamour Unleashed: Meet Our Stylists</h3>
-                   <h2>Our Stylists</h2>
-                   
-                </div>
-                
-                    <?php
-            if ($agent_array):
-                $data_wow_delay = 2;
-                ?>
-                <ul class="team_members row">
+<section id="team" class="team_section bd-bottom padding">
+    <div class="container">
+        <div class="section_heading text-center mb-40 wow fadeInUp" data-wow-delay="300ms">
+            <h3>Glamour Unleashed: Meet Our Stylists</h3>
+            <h2>Our Stylists</h2>
+
+        </div>
+
+        <?php
+        if ($agent_array):
+            $data_wow_delay = 2;
+            ?>
+            <ul class="team_members row">
                 <?php
-                $a=1;
+                $a = 1;
                 foreach ($agent_array as $agent):
 
-                   if(!empty($agent['agentImage'])): 
+                    if (!empty($agent['agentImage'])):
                         $path = SITE_BOOK_URL . 'application/uploads/img/agents/' . $agent['agentImage'];
                     else:
                         $path = 'img/logo.jpg';
@@ -188,33 +198,34 @@ if ($agenttable_res = @mysqli_query($link, $agenttable_sql)) {
                         </div>
                     </li>
                     <?php
-                    if($a%4==0){
+                    if ($a % 4 == 0) {
                         ?>
                     </ul><ul class="team_members row padding">
-                    <?php }
+                    <?php
+                    }
                     $data_wow_delay++;
                     $a++;
                 endforeach;
             endif;
             ?>
-                </ul><!-- /.team_members -->
+        </ul><!-- /.team_members -->
+    </div>
+</section><!-- /.team_section -->
+
+<section class="cta_section padding">
+    <div class="container">
+        <div class="display-table">
+            <div class="table-cel">
+                <div class="cta_content align-center wow fadeInUp" data-wow-delay="300ms">
+                    <h2>Experience Empire Salon's Magic;<br>Transform with Confidence</h2>
+                    <p>Elevate your allure with Empire salon's magic touch. Reserve your appointment now for a radiant transformation and newfound confidence!</p>
+                    <a href="book.php" class="default_btn">Make Appointment</a>
+                </div>
             </div>
-        </section><!-- /.team_section -->
-        
-       <section class="cta_section padding">
-           <div class="container">
-               <div class="display-table">
-                   <div class="table-cel">
-                       <div class="cta_content align-center wow fadeInUp" data-wow-delay="300ms">
-                           <h2>Experience Empire Salon's Magic;<br>Transform with Confidence</h2>
-                           <p>Elevate your allure with Empire salon's magic touch. Reserve your appointment now for a radiant transformation and newfound confidence!</p>
-                           <a href="book.php" class="default_btn">Make Appointment</a>
-                       </div>
-                   </div>
-               </div>
-           </div>
-       </section><!-- /.cta_section -->
-         
-        
-		
+        </div>
+    </div>
+</section><!-- /.cta_section -->
+
+
+
 <?php include_once( './includes/footer.php' ); ?>
