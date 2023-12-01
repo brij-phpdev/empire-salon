e4727580faf0dfecd44a1f9d9815263f442c623e
shriradharani <brij.phpdev@gmail.com>
2023-12-01 23:05:19 +0530

    map update

--- contact.php
+++ contact.php
@@ -2,7 +2,9 @@
 include_once './includes/header.php';
 ?>
         
-        <div class="mapouter"><div class="gmap_canvas"><iframe width="100%" height="350" id="gmap_canvas" src="https://maps.google.com/maps?q=Empire%20Salon%20Agra&t=&z=11&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.emojilib.com"></a></div><style>.mapouter{position:relative;text-align:right;height:350px;width:100%;}.gmap_canvas {overflow:hidden;background:none!important;height:350px;width:100%;}</style></div>
+        <div class="mapouter"><div class="gmap_canvas">
+                <iframe width="100%" height="350" id="gmap_canvas" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3548.53409146843!2d78.00055027445248!3d27.202376547584315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39747743f21aaaab%3A0xc9f4dadc5c470613!2sEmpire%20Salon!5e0!3m2!1sen!2sin!4v1701451862284!5m2!1sen!2sin" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div><style>.mapouter{position:relative;text-align:right;height:350px;width:100%;}.gmap_canvas {overflow:hidden;background:none!important;height:350px;width:100%;}</style></div>
 		
 		<section class="contact-section padding">
             <div class="map"></div>
@@ -13,7 +15,8 @@ include_once './includes/header.php';
 		                    <h2>Get in touch with us & <br>send us message today!</h2>
 		                    <p></p>
 		                    <h3><?php echo ADDRESS ?></h3>
-		                    <h4><span>Email:</span> <?php echo EMAIL ?> <br> <span>Phone:</span> <?php echo PHONE ?> <br> </h4>
+		                    <h4><span>Email:</span> <a class="mail_call" href="mailto:<?php echo EMAIL ?>"><?php echo EMAIL ?></a>
+                                        <br> <span>Phone:</span> <a class="mail_call" href="tel:<?php echo PHONE ?>"><?php echo PHONE ?></a> <br> </h4>
 		                </div>
 		            </div>
 		            <div class="col-lg-6 sm-padding">
---
+++ includes/mail_header.php
@@ -0,0 +1,97 @@
+<?php 
+require_once( 'config.php' );
+require_once( 'functions.php' );
+//require_once( './couch-admin/cms.php' );
+?>
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Empire Salon">
+        <meta name="author" content="The Royals">
+
+        <title>Empire Salon</title>
+
+		<link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+
+        <!-- Elegant Font Icons CSS -->
+        <link rel="stylesheet" href="css/elegant-font-icons.css">
+        <!-- Elegant Line Icons CSS -->
+        <link rel="stylesheet" href="css/elegant-line-icons.css">
+        <!-- Themify Icon CSS -->
+        <link rel="stylesheet" href="css/themify-icons.css">
+        <!-- Barber Icons CSS -->
+        <link rel="stylesheet" href="css/barber-icons.css">
+		<!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- animate CSS -->
+        <link rel="stylesheet" href="css/animate.min.css">
+		<!-- Venobox CSS -->
+        <link rel="stylesheet" href="css/venobox/venobox.css">
+        <!-- Nice Select CSS -->
+        <link rel="stylesheet" href="css/nice-select.css">
+		<!-- OWL-Carousel CSS -->
+        <link rel="stylesheet" href="css/owl.carousel.css">
+        <!-- Slick Nav CSS -->
+        <link rel="stylesheet" href="css/slicknav.min.css">
+		<!-- Main CSS -->
+        <link rel="stylesheet" href="css/main.css">
+		<!-- Responsive CSS -->
+        <link rel="stylesheet" href="css/responsive.css">
+        
+        <link rel="stylesheet" href="css/marquee.css">
+        <!--<link rel="stylesheet" href="css/calendar.css">-->
+        
+        <link rel="stylesheet" href="css/book.css">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+
+        <div id='preloader' class="d-none">
+            <div class='loader' >
+                <img src="img/logo.jpg" width="90" alt="Empire Salon">
+            </div>
+        </div> 
+        <!--        Preloader -->
+
+        <header id="header" class="header-section">
+            <div class="container">
+                <nav class="navbar ">
+                    <a href="index.php" class="navbar-brand"><img class="main_logo" src="img/logo.png" alt="Empire Salon"></a>
+                    <div class="d-flex menu-wrap align-items-center">
+                       <div id="mainmenu" class="mainmenu">
+                           <ul class="nav">
+                               <li><a data-scroll class="nav-link active" href="index.php">Home</a>
+                                </li>
+                                <li><a href="about-us.php">About</a>
+                                </li>
+                                <li><a href="<?php echo SHOP_URL ?>" target="_blank">Shop</a></li>
+                                <!--<li><a data-scroll href="#header" class="verifyOTP open-popup-link" data-link="<?php echo SHOP_URL ?>" target="_blank" >Shop</a></li>-->
+                                <li><a href="services.php">Services</a>
+                                </li>
+                                <!--<li><a href="makeover-gallery.php">Makeover</a></li>-->
+                                <li><a href="#">Packages</a>
+                                    <ul>
+                                       <li><a href="bride-packages.php">Bride Packages</a></li>
+                                       <li><a href="groom-packages.php">Groom Packages</a></li>
+                                       <li><a href="book.php">Standard Packages</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="franchise.php">Franchise</a></li>
+                                <!--<li><a href="blog-grid.php">Blog</a>-->
+                                </li>
+                                <li><a href="contact.php">Contact</a></li>
+                            </ul>
+                       </div>
+                       <div class="header-btn">
+                           <a href="book.php" class="menu-btn">Make Appointment</a>
+                       </div>
+                    </div>
+                </nav>
+            </div>
+		</header> <!--.header-section -->
