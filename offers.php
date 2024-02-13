ac2879babfdbaccce9fd339d85c5bc8ad4b4e8ab
shriradharani <brij.phpdev@gmail.com>
2024-02-14 03:34:11 +0530

    valentine offers

---
+++ offers.php
@@ -0,0 +1,174 @@
+<?php
+include_once( './includes/header.php' );
+include_once( './includes/database.php' );
+?>
+
+<section class="page_header d-flex align-items-center">
+    <div class="container">
+        <div class="section_heading text-center mb-40 wow fadeInUp" data-wow-delay="300ms">
+            <h3>Valentine's Pampering</h3>
+            <h2>OFFERS! OFFERS! OFFERS!</h2>
+
+        </div>
+    </div>
+</section><!--/. page_header -->
+
+<section class="about_section bd-bottom padding">
+    <div class="container">
+        <div class="row d-flex align-items-center">
+<!--            <div class="col-md-6 xs-padding wow fadeInRight" data-wow-delay="300ms">
+                <div class="about_video">
+                    <img class="wow fadeInUp img-fluid " data-wow-delay="500ms" src="img/about/our_experts.jpeg" alt="about">
+                </div>
+            </div>-->
+            <div class="col-md-12 xs-padding wow fadeInLeft" data-wow-delay="300ms">
+                <div class="section_heading">
+                    <h3>Offer:</h3>
+                    <div class="text-justify" style="width: 100%;text-wrap: balance;"> Enjoy a luxurious pampering experience for you and your loved one at our salon, coupled with high-quality wellness products for grooming.</div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</section><!--/. about_section -->
+
+<section class="service_section bg-grey bd-bottom padding">
+    <div class="container">
+        <div class="section_heading mb-40 wow fadeInUp" data-wow-delay="300ms">
+            <h3>Package Includes:</h3>
+            <!--<h2>Our Trending Services</h2>-->
+
+        </div>
+        <div id="trending_cuts_carousel_od" class="row ">
+            <div class="col-lg-12 col-md-12 sm-padding">
+                <div class="service_content wow fadeInUp" data-wow-delay="200ms">
+                    <h3>1. Pampering day :</h3>
+                    <P class="text-justify">Indulge in a relaxing scalp massage or facial treatment.</P>
+                </div>
+            </div>
+            <div class="col-lg-12 col-md-12 sm-padding">
+                <div class="service_content wow fadeInUp" data-wow-delay="200ms">
+                    <h3>2.⁠ ⁠Personalized Grooming Services:</h3>
+                    <P class="text-justify">Choose from a range of grooming services a tailored to your needs, including hair styling, manicure, pedicure, and more.</P>
+                </div>
+            </div>
+            <div class="col-lg-12 col-md-12 sm-padding">
+                <div class="service_content wow fadeInUp" data-wow-delay="200ms">
+                    <h3>3.⁠ ⁠Complimentary Gift:</h3>
+                    <P class="text-justify">Receive a complimentary luxury wellness product from our exclusive collection to continue your pampering experience.</P>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</section><!--/. service_section -->
+
+<section class="discount_coupons_section bd-bottom padding">
+    <div class="container">
+        <div class="section_heading mb-40 wow fadeInUp" data-wow-delay="300ms">
+            <h3>Discount Coupons:</h3>
+
+        </div>
+        <div id="trending_cuts_carousel_od" class="row ">
+            <div class="col-lg-12 col-md-12 sm-padding">
+                <div class="service_content wow fadeInUp" data-wow-delay="200ms">
+                    
+                    <p class="text-justify">
+                    <ul style="list-style: inside;">
+                        <li><i class="text-danger">20% off</i> the total price for the <span class="bg-danger text-white">Valentine's Day Pampering Package.</span></li>
+                        <li>Additional <span class="text-danger">10% off</span> when purchasing any wellness product from our website.</li>
+                    </ul>
+                    </p>
+                </div>
+            </div>
+            
+            
+        </div>
+    </div>
+</section><!--/. service_section -->
+
+<section class="discount_coupons_section bg-grey bd-bottom padding">
+    <div class="container">
+        <div class="section_heading mb-40 wow fadeInUp" data-wow-delay="300ms">
+            <h3>Validity: </h3>
+            
+        </div>
+        <div id="trending_cuts_carousel_od" class="row ">
+            <div class="col-lg-12 col-md-12 sm-padding">
+                <div class="service_content wow fadeInUp" data-wow-delay="200ms">
+                    
+                    <p class="text-justify">Offer valid for bookings made between <u>February 10<sup>th</sup></u> to <u>February 20<sup>th</sup>, 2024</u>.</p>
+                    </p>
+                </div>
+            </div>
+            
+            
+        </div>
+    </div>
+</section><!--/. service_section -->
+<section class="discount_coupons_section  bd-bottom padding">
+    <div class="container">
+        <div class="section_heading mb-40 wow fadeInUp" data-wow-delay="300ms">
+            <h3>How to Redeem:</h3>
+
+        </div>
+        <div id="trending_cuts_carousel_od" class="row ">
+            <div class="col-lg-12 col-md-12 sm-padding">
+                <div class="service_content wow fadeInUp" data-wow-delay="200ms">
+                    
+
+                    <ol class="text-justify">
+                        <li>⁠Visit our website and book your appointment for the Valentine's Day Pampering Package.</li>
+                        <li>Use the discount code <span class="text-capitalize text-white bg-success">"LOVE2024"</span> at checkout to avail the <span class="text-capitalize text-white bg-danger">20% discount</span>.</li>
+                        <li>⁠Receive a unique discount coupon code via email to use for your wellness product purchase on our website.</li>
+                    </ol>
+                </div>
+            </div>
+            
+            
+        </div>
+    </div>
+</section><!--/. service_section -->
+
+<section class="discount_coupons_section bg-grey bd-bottom padding">
+    <div class="container">
+        <div class="section_heading mb-40 wow fadeInUp" data-wow-delay="300ms">
+            <h3>Terms and Conditions:</h3>
+            
+        </div>
+        <div id="trending_cuts_carousel_od" class="row ">
+            <div class="col-lg-12 col-md-12 sm-padding">
+                <div class="service_content wow fadeInUp" data-wow-delay="200ms">
+                    
+                    <ul style="list-style: inside;">
+                        <li>⁠Offer valid for new bookings only.</li>
+                        <li>Cannot be combined with any other promotions or discounts.</li>
+                        <li>Discount coupons for wellness products valid for one-time use only and must be redeemed by <u>February 28<sup>th</sup>, 2024</u></li>
+                        <li>Subject to availability.</li>
+                    </ul>
+                </div>
+            </div>
+            
+            
+        </div>
+    </div>
+</section><!--/. service_section -->
+
+
+<section class="cta_section padding">
+    <div class="container">
+        <div class="display-table">
+            <div class="table-cel">
+                <div class="cta_content align-center wow fadeInUp" data-wow-delay="300ms">
+                    <h2>Experience Empire Salon's Magic;<br>Transform with Confidence</h2>
+                    <p>Elevate your allure with Empire salon's magic touch. Reserve your appointment now for a radiant transformation and newfound confidence!</p>
+                    <a href="book.php" class="default_btn">Make Appointment</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><!-- /.cta_section -->
+
+
+
+<?php include_once( './includes/footer.php' ); ?>
