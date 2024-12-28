0a848fdf715a485b61aafd597c6dfa55a29dca47
shriradharani <brij.phpdev@gmail.com>
2024-12-28 15:12:23 +0530

    update location

--- booking_post.php
+++ booking_post.php
@@ -2,7 +2,7 @@
 @session_start();
 //error_reporting(E_ALL);
 //ini_set('display_errors',1);
-//include_once __DIR__.'/config.php';
+include_once __DIR__.'/config.php';
 include_once './includes/database.php';
 include_once './includes/function.php';
 //print_r($_POST);
